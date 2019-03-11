a7a844008acfffd8a1984fdaadae50797243494c
zzr716 <z71634@163.com>
2019-03-11 10:31:46 +0800

    重置

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //index.js
 //获取应用实例
 const app = getApp()
@@ -53,59 +52,3 @@ Page({
     })
   }
 })
-=======
-//index.js
-//获取应用实例
-const app = getApp()
-
-Page({
-  data: {
-    motto: '测试201s9',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
->>>>>>> 9014739c654e4fb7c99637c7efe21dfc8f17f306
